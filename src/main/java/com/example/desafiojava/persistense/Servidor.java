2db00cf0f3d04869fd443d8cf10a6f970a8fc829
Williamrcf <william.cavalcante@ifood.com.br>
2022-06-28 16:36:39 -0300

    Configuração da camada de persistência.

---
+++ src/main/java/com/example/desafiojava/persistense/Servidor.java
@@ -0,0 +1,26 @@
+package com.example.desafiojava.persistense;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.sql.Date;
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor
+@Data
+@Table (name = "servidor")
+public class Servidor {
+
+    @Id
+    public UUID matricula;
+    @Column
+    public String nome;
+    @Column (name = "data_de_cadastro")
+    public Date dataDeCadastro;
+    @Column
+    public UUID lotacao;
+}
